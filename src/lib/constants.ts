4062046c6da859289907959a62f838d642d00460
Stephen Coloma <2232847@slu.edu.ph>
2025-05-07 00:52:27 +0800

    feat(file upload) preparing data to send as zip
    This commit uses the JSzip library to zip the dicom files before sending to the server.
    Implemented the usePost custom hook to facilitate the requests to the server
    Moved important constants in the constant folder for cleaner code

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const API_BASE_URL = `http://` + process.env.NEXT_PUBLIC_SERVER_HOST;
+
+export const MIN_SLICES = 32; // minimum number of slices from user input
+
+export const ZIP_FILE_NAME = 'dicom_files.zip'
+
+export const ZIPPED_SERVER_FIELD_NAME = 'zippedDicom'
